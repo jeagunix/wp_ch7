12f22f8f3c78d33fc9e25da95c6de6130b6f5063
venture21 <park0122@gmail.com>
2019-11-06 11:46:57 +0900

    jquery #2

---
+++ WebContent/tabbox.js
@@ -0,0 +1,27 @@
+/*
+var tabBox = {
+	onTabClicked: function(event){
+		var lisSelected = document.getElementsByClassName("selected");
+		(lisSelected.length == 1) && (lisSelected[0].className = "");
+		this.className = "selected";
+	},		
+	init: function(){
+		console.log(this);
+		var div = document.getElementsByClassName("tab-box")[0];
+		var ul  = div.childNodes[1];
+		var lis = ul.getElementsByTagName("li");
+		for(var i = 0; i < lis.length; i++){
+			lis[i].addEventListener("click", this.onTabClicked);
+		}		
+	}
+}
+*/
+var tabBox = {
+	onTabClicked: function(event){
+		$(".selected").removeClass('selected');
+		$(this).addClass('selected');
+	},		
+	init: function(){
+		$(".tab-box ul li").click(this.onTabClicked);
+	}
+}
